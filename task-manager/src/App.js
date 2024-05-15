05d2d4457b93230ce68271c1a8d575affe3aa26c
PRUTHVI GADHAVE <pruthvi.github25@gmail.com>
2024-05-15 22:33:29 +0530

    route on sidebar

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -1,19 +1,26 @@
 
 import './App.css';
 import Sidebar from './components/Sidebar';
+import { Router, Routes, Route} from "react-router-dom"
+import TaskList from './components/TaskList';
+import TimeTracker from './components/TimeTracker';
 
 function App() {
   return (
- <>
- <div className='landing-page d-flex'>
-  <div className='w-25'>
-  <Sidebar/>
-  </div>
-  <div className='w-75'>Right</div>
- 
- </div>
   
- </>
+    <div className='landing-page d-flex'>
+      <div className='w-25'>
+        <Sidebar />
+      </div>
+      <div className='w-75'>
+
+        <Routes>
+          <Route path="/" element={<TaskList />} />          
+          <Route exact path="/timetracker" element={<TimeTracker />} />          
+      </Routes>
+      </div>
+    </div>
+ 
   );
 }
 
--- task-manager/src/components/Sidebar.jsx
+++ task-manager/src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Sidebar.css"
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Sidebar() {
     return (
@@ -13,21 +14,21 @@ export default function Sidebar() {
                     <hr />
                     <ul className="nav nav-pills flex-column mb-auto">
                         <li className="nav-item">
-                            <a href="#" className="nav-link active" aria-current="page">
+                            <NavLink   activeClassName="active"  to="/" href="#"  className="nav-link " aria-current="page">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="29" fill="currentColor" className="bi bi-card-checklist me-1" viewBox="0 0 16 16">
                                     <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z" />
                                     <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0M7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0" />
                                 </svg>
                                 Task
-                            </a>
+                            </NavLink >
                         </li>
                         <li>
-                            <a href="#" className="nav-link link-light">
+                            <NavLink  to="/timetracker" href="#" activeClassName="active" className="nav-link link-light">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="35" fill="currentColor" className="bi bi-stopwatch-fill me-1" viewBox="0 0 16 16">
                                     <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07A7.001 7.001 0 0 0 8 16a7 7 0 0 0 5.29-11.584l.013-.012.354-.354.353.354a.5.5 0 1 0 .707-.707l-1.414-1.415a.5.5 0 1 0-.707.707l.354.354-.354.354-.012.012A6.97 6.97 0 0 0 9 2.071V1h.5a.5.5 0 0 0 0-1zm2 5.6V9a.5.5 0 0 1-.5.5H4.5a.5.5 0 0 1 0-1h3V5.6a.5.5 0 1 1 1 0" />
                                 </svg>
                                 Time Tracking
-                            </a>
+                            </NavLink >
                         </li>
 
                     </ul>
